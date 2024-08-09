140d58cfa93cdc2101577107b34ace066588994c
Meraki <786376908@qq.com>
2024-08-09 17:01:18 +0800

    引入了Kan网络，并且完善了输出信息

--- model/model.py
+++ model/model.py
@@ -3,7 +3,21 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from typing import Optional
+from kan import *
 
+
+class Kan_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.kan1 = KAN(width=[52,64,128], grid=3, k=3, seed=42)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.kan2 = KAN(width=[128,64,25], grid=3, k=3, seed=42)
+
+    def forward(self, x):
+        x = self.bn1(self.kan1(x))
+        x = self.kan2(x)
+
+        return x
 
 class Model_bs_mlp(nn.Module):
     def __init__(self):
@@ -14,7 +28,7 @@ class Model_bs_mlp(nn.Module):
         self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 64)
         self.bn3 = nn.BatchNorm1d(64)
-        self.fc4 = nn.Linear(64, 14)  # 输出层对应 14 个电机
+        self.fc4 = nn.Linear(64, 25)  # 输出层对应 14 个电机
 
         self.res1 = nn.Linear(52, 256)
         self.res2 = nn.Linear(256, 64)
