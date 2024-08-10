39e75bf1eb963b168f5f444938b5c992c22230f5
Meraki <786376908@qq.com>
2024-08-10 15:36:34 +0800

    修改了Kan模型,把MLP v2改到四层

--- model/model.py
+++ model/model.py
@@ -8,8 +8,8 @@ from kan import *
 
 class Kan_Model(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.kan1 = KAN(width=[52,64,128], grid=3, k=3, seed=42)
+        super(Kan_Model,self).__init__()
+        self.kan1 = KAN(width=[52,128], grid=3, k=3, seed=42)
         self.bn1 = nn.BatchNorm1d(128)
         self.kan2 = KAN(width=[128,64,25], grid=3, k=3, seed=42)
 
@@ -65,7 +65,7 @@ class Model_bs_mlp_v2(nn.Module):
 
         self.fc5 = nn.Linear(64,25)
 
-        self.drout = nn.Dropout(0.0)
+        self.drout = nn.Dropout(0.2)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
@@ -73,8 +73,8 @@ class Model_bs_mlp_v2(nn.Module):
 
     def forward(self, x):
         x = self.drout(self.relu(self.bn1(self.fc1(x))))
-        # x = self.drout(self.relu(self.bn2(self.fc2(x))))
-        # x = self.drout(self.relu(self.bn3(self.fc3(x))))
+        x = self.drout(self.relu(self.bn2(self.fc2(x))))
+        x = self.drout(self.relu(self.bn3(self.fc3(x))))
         x = self.drout(self.relu(self.bn4(self.fc4(x))))
         x = self.sigmoid(self.fc5(x))
 
